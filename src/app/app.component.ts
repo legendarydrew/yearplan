40ee5c0c73e075d50e9c41f4bcfc2a7cbd6f7ce6
Drew Maughan <drew.maughan@toptal.com>
2024-12-26 13:42:23 +0000

    feat: save events to localStorage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { EventsEditorComponent } from './components/events-editor/events-editor.component';
+import { CalendarEvent } from './interfaces';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,19 @@ import { EventsEditorComponent } from './components/events-editor/events-editor.
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'yearplan';
+
+  eventsList: CalendarEvent[] = [];
+
+  ngOnInit() {
+    // Fetch any saved events from localStorage.
+    const yp = localStorage.getItem('yp');
+    this.eventsList = yp ? JSON.parse(yp)['events'] : [];
+  }
+
+  updateEventsHandler(events: CalendarEvent[]): void {
+    this.eventsList = events;
+    localStorage.setItem('yp', JSON.stringify({ events: events }));
+  }
 }
